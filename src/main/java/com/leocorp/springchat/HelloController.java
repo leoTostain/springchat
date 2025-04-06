94deb77b8698cfbcdd7cd5f73f0faa53f1d688a4
leoTostain <leo.tostain@gmail.com>
2025-04-06 16:07:35 +0200

    fix: Add CrossOrigin annotation to Controller

--- src/main/java/com/leocorp/springchat/HelloController.java
+++ src/main/java/com/leocorp/springchat/HelloController.java
@@ -1,10 +1,12 @@
 package com.leocorp.springchat;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
+@CrossOrigin
 @RestController
 public class HelloController {
     public record MyRecord(String name, LocalDate date) { }
