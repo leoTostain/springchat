52288bd50d2b6ef88ec79bf4b003b97ef3539744
leoTostain <leo.tostain@gmail.com>
2025-04-12 19:58:52 +0200

    fix: populate database for junit tests and add some tests

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USER_ENTITY (uuid, username) VALUES ('dde47cf4-973a-45b3-b72f-39871c6c53de', 'user1');
+INSERT INTO USER_ENTITY (uuid, username) VALUES ('957cbde3-7673-4bcc-8aa5-e6f861087a83', 'user2');
+INSERT INTO USER_ENTITY (uuid, username) VALUES ('034dbe7c-99af-41ac-8907-8318189a6eee', 'user3');
+INSERT INTO USER_ENTITY (uuid, username) VALUES ('ea5b550e-0b1f-4f51-bb7f-b4c3e023abc6', 'user4');
