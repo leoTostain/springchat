b8e5cb0fcc4135a61a639be0d4172d64582c7751
leoTostain <leo.tostain@gmail.com>
2025-04-12 19:58:09 +0200

    fix: auto-increment id

--- src/main/java/com/leocorp/springchat/user/dao/UserEntity.java
+++ src/main/java/com/leocorp/springchat/user/dao/UserEntity.java
@@ -1,9 +1,6 @@
 package com.leocorp.springchat.user.dao;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -11,7 +8,7 @@ import java.util.UUID;
 @Entity
 public class UserEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
