8671967e805dae6637eea3edd53f670460a8a2fb
leoTostain <leo.tostain@gmail.com>
2025-04-27 14:46:47 +0200

    feat: add data.sql with admin user

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER_ENTITY (uuid, username, password) VALUES ('b0dfe8fb-ca12-4d7d-aa3e-167f0784a39f',
+                                                           'admin',
+                                                           '{bcrypt}$2y$10$EMAIg0MLv6xnl3wf3jzamO1jcYbC06GLysAKW.eoRiujlWJN73haS');
+
+INSERT INTO authority_entity (user_id, authority) VALUES ('1', 'ROLE_ADMIN');
