9d6f7fdc6aae1b25593917697abfaa3db007173d
leoTostain <leo.tostain@gmail.com>
2025-04-29 14:06:34 +0200

    feat: add postgres db with docker

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,10 @@
-INSERT INTO USER_ENTITY (uuid, username, password) VALUES ('b0dfe8fb-ca12-4d7d-aa3e-167f0784a39f',
-                                                           'admin',
-                                                           '{bcrypt}$2y$10$EMAIg0MLv6xnl3wf3jzamO1jcYbC06GLysAKW.eoRiujlWJN73haS');
+INSERT INTO USER_ENTITY (uuid, username, password)
+       VALUES ('b0dfe8fb-ca12-4d7d-aa3e-167f0784a39f',
+        'admin',
+        '{bcrypt}$2y$10$EMAIg0MLv6xnl3wf3jzamO1jcYbC06GLysAKW.eoRiujlWJN73haS')
+       ON CONFLICT (username) DO NOTHING;
 
-INSERT INTO authority_entity (user_id, authority) VALUES ('1', 'ROLE_ADMIN');
+INSERT INTO authority_entity (user_id, authority)
+       VALUES ('1', 'ROLE_ADMIN')
+       ON CONFLICT (user_id, authority) DO NOTHING;
+
