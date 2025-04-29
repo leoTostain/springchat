51ad9d5cb27e35db5ce179d072b664527265a4d4
leoTostain <leo.tostain@gmail.com>
2025-04-29 10:38:30 +0200

    feat: add default constructor

--- src/main/java/com/leocorp/springchat/user/exception/UserNotFoundException.java
+++ src/main/java/com/leocorp/springchat/user/exception/UserNotFoundException.java
@@ -3,8 +3,12 @@ package com.leocorp.springchat.user.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No user found for the given uuid")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found")
 public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("User not found");
+    }
+
     public UserNotFoundException(String message) {
         super(message);
     }
