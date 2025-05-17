6495fe1d3a6ca374c4720be14002130de196e978
leoTostain <leo.tostain@gmail.com>
2025-05-17 13:50:17 +0200

    fix: update endpoint

--- src/main/java/com/leocorp/springchat/HomeController.java
+++ src/main/java/com/leocorp/springchat/HomeController.java
@@ -1,13 +1,13 @@
 package com.leocorp.springchat;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/greetings")
     public @ResponseBody String greeting() {
         return "Hello, World";
     }
